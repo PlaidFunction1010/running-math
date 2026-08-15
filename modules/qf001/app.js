c6df8c3d0071927529cc3eb44850c75053e8d979
PlaidFunction1010 <kahootgogogo@gmail.com>
2026-08-15 14:25:18 +0800

    Update app.js

--- modules/qf001/app.js
+++ modules/qf001/app.js
@@ -1,18 +1,2 @@
-const conditionsDiv = document.getElementById("conditions");
-
-const aSign = Math.random() < 0.5 ? ">" : "<";
-
-const bPool = [">", "=", "<"];
-const cPool = [">", "=", "<"];
-
-const bSign = bPool[Math.floor(Math.random() * 3)];
-const cSign = cPool[Math.floor(Math.random() * 3)];
-
-conditionsDiv.innerHTML = `
-a ${aSign} 0<br>
-b ${bSign} 0<br>
-c ${cSign} 0
-`;
-
-document.getElementById("result").innerHTML =
-"題目生成成功";
+document.getElementById("conditions").innerHTML =
+Math.random();
