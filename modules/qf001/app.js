24a44f77900f6974e7dd1269ffea5c2d8a55c306
PlaidFunction1010 <kahootgogogo@gmail.com>
2026-08-15 14:19:03 +0800

    Update app.js

--- modules/qf001/app.js
+++ modules/qf001/app.js
@@ -1,1 +1,10 @@
-modules/qf001/app.js
+const conditionsDiv = document.getElementById("conditions");
+
+conditionsDiv.innerHTML = `
+a > 0<br>
+b < 0<br>
+c = 0
+`;
+
+document.getElementById("result").innerHTML =
+"QF-001 模組載入成功";
